7a534897931d04f8f8d7f4e8523b0298a45c9271
Charlotte Dawson <csdaw@outlook.com>
2021-03-18 14:29:42 +0000

    Export modifiers

--- R/modifiers.R
+++ R/modifiers.R
@@ -1,6 +1,35 @@
-fixed <- function(x) {
+#' Control pattern matching with modifier functions
+#'
+#' @description
+#' \describe{
+#'   \item{`fixed()`}{Match the string literally.}
+#'   \item{`regex()`}{Match using a POSIX 1003.2 extended regular expression.}
+#'   \item{`perl()`}{The default, match using a perl-compatible regular expression.}
+#' }
+#'
+#' @param pattern `character`, a pattern to modify the behaviour of.
+#' @param ignore_case `logical`, should case-insensitive matching be used?
+#' Default is `FALSE`.
+#' @name modifiers
+#'
+#' @examples
+#'
+#' @export
+perl <- function(pattern, ignore_case = FALSE) {
   structure(
-    x,
+    pattern,
+    ignore_case = ignore_case,
+    class = c("stringrb_perl", "stringr_pattern", "character")
+  )
+}
+
+is_perl <- function(x) inherits(x, "stringrb_perl") || is.null(attr(x, "class"))
+
+#' @rdname modifiers
+#' @export
+fixed <- function(pattern) {
+  structure(
+    pattern,
     class = c("stringrb_fixed", "stringr_pattern", "character")
   )
 }
@@ -9,19 +38,11 @@ is_fixed <- function(x) inherits(x, "stringrb_fixed")
 
 ignore_case <- function(x) isTRUE(attr(x, "ignore_case"))
 
-perl <- function(x, ignore_case = FALSE) {
+#' @rdname modifiers
+#' @export
+regex <- function(pattern, ignore_case = FALSE) {
   structure(
-    x,
-    ignore_case = ignore_case,
-    class = c("stringrb_perl", "stringr_pattern", "character")
-  )
-}
-
-is_perl <- function(x) inherits(x, "stringrb_perl") || is.null(attr(x, "class"))
-
-regex <- function(x, ignore_case = FALSE) {
-  structure(
-    x,
+    pattern,
     ignore_case = ignore_case,
     class = c("stringrb_regex", "stringr_pattern", "character")
   )
