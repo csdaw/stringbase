b9c9b389905a1ca00e9bb8f7d9b50bdba80c64a3
Charlotte Dawson <csdaw@outlook.com>
2021-03-18 09:55:19 +0000

    Document str_extract()

--- R/extract.R
+++ R/extract.R
@@ -1,3 +1,23 @@
+#' Extract matching patterns from a string
+#'
+#' @description Vectorised over `string`, but not `pattern` which must be
+#' a single string (unlike stringr).
+#'
+#' @param string `character vector` of strings.
+#' @param pattern `character`, a pattern to match. Can be:
+#'   * A Perl-compatible regular expression (default).
+#'   * Wrap with `perl(ignore_case = TRUE)` to use case-insensitive matching.
+#'   * Wrap with `fixed()` to use a fixed/literal match.
+#'   * Wrap with `regex()` to use a POSIX 1003.2 extended regular expression.
+#'   * Wrap with `regex(ignore_case = TRUE)` to use case-insensitive matching with
+#'     a POSIX 1003.2 extended regular expression.
+#'
+#' @return Returns a `character vector` the same length as `string`. Non-matching
+#' elements will be replaced with `NA`.
+#'
+#' @examples
+#'
+#' @export
 str_extract <- function(string, pattern) {
   check_pattern(pattern)
 
