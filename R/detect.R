a22157d0335bd16107060c439f148ba3494f0b14
Charlotte Dawson <csdaw@outlook.com>
2021-03-26 11:28:28 +0000

    Add str_starts() and str_ends()

--- R/detect.R
+++ R/detect.R
@@ -45,3 +45,51 @@ str_detect <- function(string, pattern, negate = FALSE) {
     out
   }
 }
+
+#' Detect the presence or absence of a pattern at the beginning or end of a
+#' string.
+#'
+#' @description Vectorised over `string`, but not `pattern` which must be
+#' a single string (unlike stringr).
+#'
+#' @param string `character vector` of strings.
+#' @param pattern `character`, a pattern with which the string should start
+#' or end.
+#'
+#' Can be:
+#'   * A Perl-compatible regular expression (default).
+#'   * Wrap with `perl(ignore_case = TRUE)` to use case-insensitive matching.
+#'   * Wrap with `fixed()` to use a fixed/literal match.
+#'   * Wrap with `regex()` to use a POSIX 1003.2 extended regular expression.
+#'   * Wrap with `regex(ignore_case = TRUE)` to use case-insensitive matching with
+#'     a POSIX 1003.2 extended regular expression.
+#' @param negate `logical`, if `TRUE` return non-matching elements. Default is
+#' `FALSE`.
+#'
+#' @return Returns a `logical vector` the same length as `string`.
+#' @seealso [str_detect()] which this function wraps.
+#' @examples
+#' fruit <- c("apple", "banana", "pear", "pineapple")
+#' str_starts(fruit, "p")
+#' str_starts(fruit, "p", negate = TRUE)
+#' str_ends(fruit, "e")
+#' str_ends(fruit, "e", negate = TRUE)
+#'
+#' @export
+str_starts <- function(string, pattern, negate = FALSE) {
+  check_pattern(pattern)
+
+  pattern2 <- paste0("^(", pattern, ")")
+  attributes(pattern2) <- attributes(pattern)
+  str_detect(string = string, pattern = pattern2, negate = negate)
+}
+
+#' @rdname str_starts
+#' @export
+str_ends <- function(string, pattern, negate = FALSE) {
+  check_pattern(pattern)
+
+  pattern2 <- paste0("(", pattern, ")$")
+  attributes(pattern2) <- attributes(pattern)
+  str_detect(string = string, pattern = pattern2, negate = negate)
+}
--- tests/tinytest/test-detect.R
+++ tests/tinytest/test-detect.R
@@ -9,6 +9,38 @@ expect_equal(str_detect(character(), "x"), logical())
 expect_equal(str_detect(c("a", "ab", "abc"), "c", negate = TRUE), c(T, T, F))
 
 # test that str_detect can take pattern modifiers
-expect_equal(str_detect("a", fixed(".")), FALSE)
-expect_equal(str_detect("a", regex("A", ignore_case = TRUE)), TRUE)
-expect_equal(str_detect("a", perl("(?i)A")), TRUE)
+expect_false(str_detect("a", fixed(".")))
+expect_true(str_detect("a", regex("A", ignore_case = TRUE)))
+expect_true(str_detect("a", perl("(?i)A")))
+
+# test that str_starts works
+expect_true(str_starts("ab", "a"))
+expect_false(str_starts("ab", "b"))
+
+# test that str_starts negation works
+expect_false(str_starts("ab", "a", negate = TRUE))
+expect_true(str_starts("ab", "b", negate = TRUE))
+
+# test that str_starts can take pattern modifiers
+expect_true(str_starts("ab", perl("A", ignore_case = TRUE)))
+expect_true(str_starts("ab", regex("A", ignore_case = TRUE)))
+
+# test that str_starts respects operators
+expect_true(str_starts("ab", "b|a"))
+expect_false(str_starts("ab", "c|b"))
+
+# test that str_ends works
+expect_true(str_ends("ab", "b"))
+expect_false(str_ends("ab", "a"))
+
+# test that str_ends negation works
+expect_false(str_ends("ab", "b", negate = TRUE))
+expect_true(str_ends("ab", "a", negate = TRUE))
+
+# test that str_starts can take pattern modifiers
+expect_true(str_ends("ab", perl("B", ignore_case = TRUE)))
+expect_true(str_ends("ab", regex("B", ignore_case = TRUE)))
+
+# test that str_starts respects operators
+expect_true(str_ends("ab", "b|a"))
+expect_false(str_ends("ab", "c|a"))
