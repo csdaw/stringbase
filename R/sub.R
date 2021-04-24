8e6e4b8a545615a0ebf75e53208825ad93e53730
Charlotte Dawson <csdaw@outlook.com>
2021-04-24 17:11:05 +0100

    Fix str_sub argument recycling
    Also add docs and tests

--- R/sub.R
+++ R/sub.R
@@ -1,3 +1,48 @@
+#' Extract and replace substrings from a character vector
+#'
+#' `str_sub` will recycle all arguments to be the same length as the
+#' longest argument. If any arguments are of length 0, the output will be
+#' a zero length character vector.
+#'
+#' Substrings are inclusive - they include the characters at both start and
+#' end positions. `str_sub(string, 1, -1)` will return the complete
+#' substring, from the first character to the last.
+#'
+#' Does not support replacement form (unlike stringr).
+#'
+#' @param string `character vector` of strings.
+#' @param start `integer vector` giving the position(s) of the first character,
+#' default is `1L`. Negative values count backwards from the last character.
+#' Alternatively pass a two-column `numeric matrix` e.g. the
+#' output of `str_locate_all()`.
+#' @param end `integer vector` giving the position(s) of the last character,
+#' default is `1000000L`. Negative values count backwards from the last character.
+#' @return Returns a `character vector` of substring(s) from `start` to `end`
+#' (inclusive). Will be length of longest input argument.
+#' @examples
+#' hw <- "Hadley Wickham"
+#'
+#' str_sub(hw, 1, 6)
+#' str_sub(hw, end = 6)
+#' str_sub(hw, 8, 14)
+#' str_sub(hw, 8)
+#' str_sub(hw, c(1, 8), c(6, 14))
+#'
+#' # Negative indices
+#' str_sub(hw, -1)
+#' str_sub(hw, -7)
+#' str_sub(hw, end = -7)
+#'
+#' # Alternatively, you can pass in a two column matrix, as in the
+#' # output from str_locate_all
+#' pos <- str_locate_all(hw, "[aeio]")[[1]]
+#' str_sub(hw, pos)
+#' str_sub(hw, pos[, 1], pos[, 2])
+#'
+#' # Vectorisation
+#' str_sub(hw, seq_len(nchar(hw)))
+#' str_sub(hw, end = seq_len(nchar(hw)))
+#' @export
 str_sub <- function(string, start = 1L, end = 1000000L) {
   if (is.matrix(start)) {
     end <- start[, 2]
@@ -7,15 +52,18 @@ str_sub <- function(string, start = 1L, end = 1000000L) {
   if (!any(start < 0) & !any(end < 0)) {
     substring(string, start, end)
   } else {
-    n <- max(lt <- length(string), length(start), length(end))
+    # argument recycling
+    n <- max(length(string), length(start), length(end))
+    string <- rep_len(string, n)
+    start <- rep_len(start, n)
+    end <- rep_len(end, n)
 
-    if (lt && lt < n) string <- rep_len(string, length.out = n)
-
+    # dealing with negative integers
     nchars <- nchar(string)
-
     start[start < 0] <- nchars[start < 0] + 1 + start[start < 0]
     end[end < 0] <- nchars[end < 0] + 1 + end[end < 0]
 
     substring(string, start, end)
   }
 }
+lengths
---
+++ tests/tinytest/test-sub.R
@@ -0,0 +1,57 @@
+# test that the correct substring is extracted"
+alphabet <- paste0(letters, collapse = "")
+expect_equal(str_sub(alphabet, 1, 3),
+             "abc")
+expect_equal(str_sub(alphabet, 24, 26),
+             "xyz")
+
+#test that arguments are expanded to longest
+expect_equal(str_sub(alphabet, c(1, 24), c(3, 26)),
+             c("abc", "xyz"))
+expect_equal(str_sub(c("abc", "xyz"), 2, 2),
+             c("b", "y"))
+
+# test_that start and end are recycled
+expect_equal(str_sub(c("ab", "cd"), 2, 2),
+             c("b", "d"))
+expect_equal(str_sub(character(), 1),
+             character())
+
+# test that specifying only end subsets from start
+expect_equal(str_sub(alphabet, end = 3),
+             "abc")
+
+# test_that("start and end are vectorised
+expect_equal(str_sub(c("ab", "cde"), -1, -1),
+             c("b", "e"))
+expect_equal(str_sub(c("ab", "cde"), 1, c(1, 2)),
+             c("a", "cd"))
+
+# test that can pass start a matrix
+expect_equal(str_sub("abcdefg", cbind(3, 5)),
+             "cde")
+
+# test that specifying only start subsets to end
+expect_equal(str_sub(alphabet, 24),
+             "xyz")
+
+# test_that specifying -1 as end selects entire string
+expect_equal(str_sub("ABCDEF", c(4, 5), c(5, -1)),
+             c("DE", "EF"))
+
+expect_equal(str_sub("ABCDEF", c(4, 5), c(-1, -1)),
+             c("DEF", "EF"))
+
+# test that negative values select from end
+expect_equal(str_sub("ABCDEF", 1, -4), "ABC")
+expect_equal(str_sub("ABCDEF", -3), "DEF")
+
+# test that missing arguments give missing results
+expect_equal(str_sub(NA), NA_character_)
+expect_equal(str_sub(NA, 1, 3), NA_character_)
+expect_equal(str_sub(c(NA, "NA"), 1, 3), c(NA, "NA"))
+
+expect_error(str_sub("test", NA, NA),
+             "missing value where TRUE/FALSE needed")
+expect_error(str_sub(c(NA, "test"), NA, NA),
+             "missing value where TRUE/FALSE needed")
