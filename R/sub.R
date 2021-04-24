5b7c99baf38217b63fc673a959d3b55f34b91460
Charlotte Dawson <csdaw@outlook.com>
2021-04-24 16:01:07 +0100

    Speed up basic str_sub

--- R/sub.R
+++ R/sub.R
@@ -4,15 +4,13 @@ str_sub <- function(string, start = 1L, end = 1000000L) {
   } else {
     n <- max(lt <- length(string), length(start), length(end))
 
-    if (lt && lt < n)
-      string <- rep_len(string, length.out = n)
+    if (lt && lt < n) string <- rep_len(string, length.out = n)
 
-    nchars <- sapply(string, nchar)
-    start_new <- mapply(function(n, st) ifelse(st < 0, n + 1 + st, st),
-                        nchars, start, USE.NAMES = FALSE, SIMPLIFY = TRUE)
-    end_new <- mapply(function(n, en) ifelse(en < 0, n + 1 + en, en),
-                      nchars, end, USE.NAMES = FALSE, SIMPLIFY = TRUE)
+    nchars <- nchar(string)
 
-    substring(string, start_new, end_new)
+    start[start < 0] <- nchars[start < 0] + 1 + start[start < 0]
+    end[end < 0] <- nchars[end < 0] + 1 + end[end < 0]
+
+    substring(string, start, end)
   }
 }
