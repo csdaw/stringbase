3f9b565024d397a8bea1ff167ad838d042968c20
Charlotte Dawson <csdaw@outlook.com>
2021-06-15 11:27:23 +0100

    Add str_match()

---
+++ R/match.R
@@ -0,0 +1,84 @@
+#' Extract matched groups from a string
+#'
+#' @description Vectorised over `string` and `pattern`.
+#'
+#' @inheritParams str_detect
+#' @param pattern `string` or `character vector`, pattern(s) to match. Typically
+#'   contains multiple capture groups. Can be:
+#'   * A Perl-compatible regular expression (default).
+#'   * Wrap with `perl(ignore_case = TRUE)` to use case-insensitive matching.
+#'   * Wrap with `fixed()` to use a fixed/literal match.
+#'   * Wrap with `regex()` to use a POSIX 1003.2 extended regular expression.
+#'   * Wrap with `regex(ignore_case = TRUE)` to use case-insensitive matching with
+#'     a POSIX 1003.2 extended regular expression.
+#' @return Returns a `character matrix` for `str_match()`. The first column is
+#'   the complete match, followed by one column for each capture group.
+#'   Returns a `list` of `character matrices` for `str_match_all()`.
+#'
+#' @seealso [str_extract()] to extract the complete match.
+#' @export
+#' @examples
+#' strings <- c(" 219 733 8965", "329-293-8753 ", "banana", "595 794 7569",
+#'   "387 287 6718", "apple", "233.398.9187  ", "482 952 3315",
+#'   "239 923 8115 and 842 566 4692", "Work: 579-499-7527", "$1000",
+#'   "Home: 543.355.3679")
+#' phone <- "([2-9][0-9]{2})[- .]([0-9]{3})[- .]([0-9]{4})"
+#'
+#' str_extract(strings, phone)
+#' str_match(strings, phone)
+#'
+#' # Extract/match all
+#' str_extract_all(strings, phone)
+#' #str_match_all(strings, phone)
+#'
+#' x <- c("<a> <b>", "<a> <>", "<a>", "", NA)
+#' str_match(x, "<(.*?)> <(.*?)>")
+#' #str_match_all(x, "<(.*?)>")
+#'
+#' str_extract(x, "<.*?>")
+#' str_extract_all(x, "<.*?>")
+str_match <- function(string, pattern) {
+  if (is_fixed(pattern)) stop("Can only match regular expressions")
+  check_lengths(string, pattern)
+
+  if (length(pattern) > 1) {
+    loc <- mapply(
+      function(p, s) {
+        regexec(
+          text = s,
+          pattern = p,
+          ignore.case = ignore_case(pattern),
+          perl = is_perl(pattern),
+          fixed = FALSE
+        )
+
+      },
+      pattern, string,
+      USE.NAMES = FALSE
+    )
+    out <- regmatches(
+      if (length(string) == length(pattern)) string else rep(string, length(pattern)),
+      loc
+    )
+    mat_nrow <- length(pattern)
+    mat_ncol <- if (all(is.na(string))) 2 else max(lengths(out))
+
+  } else {
+    loc <- regexec(
+      pattern = pattern,
+      text = string,
+      ignore.case = ignore_case(pattern),
+      perl = is_perl(pattern),
+      fixed = FALSE
+    )
+    out <- regmatches(string, loc)
+    mat_nrow <- length(string)
+    mat_ncol <- if (all(is.na(string))) 2 else max(lengths(out))
+  }
+  out_mat <- matrix(NA_character_, nrow = mat_nrow, ncol = mat_ncol)
+
+  for (row in which(lengths(out) != 0)) {
+    out_mat[row, ] <- out[[row]]
+  }
+  out_mat
+}
---
+++ tests/tinytest/test-match.R
@@ -0,0 +1,65 @@
+set.seed(1410)
+num <- matrix(sample(9, 10 * 10, replace = T), ncol = 10)
+num_flat <- apply(num, 1, str_c, collapse = "")
+
+phones <- str_c(
+  "(", num[, 1], num[, 2], num[, 3], ") ",
+  num[, 4], num[, 5], num[, 6], " ",
+  num[, 7], num[, 8], num[, 9], num[, 10]
+)
+
+# test that empty strings return correct matrix of correct size
+expect_equal(
+  str_match(c(NA, NA), "(a)"),
+  matrix(NA_character_, 2, 2)
+)
+
+expect_equal(
+  str_match(c(NA, NA, NA), c("(a)", "(b)", "c")),
+  matrix(NA_character_, 3, 2)
+)
+
+expect_equal(
+  str_match(character(), "(a)"),
+  matrix(character(), 0, 2)
+)
+
+expect_equal(
+  str_match(c(character(), "banana"), "(a)"),
+  matrix("a", 1, 2)
+)
+
+# test that no matching cases returns a 1 column matrix
+res <- str_match(c("a", "b"), ".")
+
+expect_equal(nrow(res), 2)
+expect_equal(ncol(res), 1)
+expect_equal(res[, 1], c("a", "b"))
+
+# test that str_match() works when all match
+matches <- str_match(phones, "\\(([0-9]{3})\\) ([0-9]{3}) ([0-9]{4})")
+
+expect_equal(nrow(matches), length(phones))
+expect_equal(ncol(matches), 4)
+
+expect_equal(matches[, 1], phones)
+
+matches_flat <- apply(matches[, -1], 1, str_c, collapse = "")
+expect_equal(matches_flat, num_flat)
+
+# test that str_match returns NA when some inputs don't match
+matches <- str_match(c(phones, "blah", NA),
+                     "\\(([0-9]{3})\\) ([0-9]{3}) ([0-9]{4})")
+
+expect_equal(nrow(matches), length(phones) + 2)
+expect_equal(ncol(matches), 4)
+
+expect_equal(matches[11, ], rep(NA_character_, 4))
+expect_equal(matches[12, ], rep(NA_character_, 4))
+
+# test that match returns empty string when optional group doesn't match
+# This is different to stringr! (which would return NA)
+expect_equal(str_match(c("ab", "a"), "(a)(b)?")[, 3], c("b", ""))
+
+# test that tidyverse recycling rules are used
+expect_error(str_match(c("a", "b"), c("a", "b", "c")))
